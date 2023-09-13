6cf75600096dd9f8ad98b535e92f6d8e908c3411
Myles Eftos <myles@madpilot.com.au>
2023-09-13 20:13:16 +1000

    Removing firmware from device types

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,3 @@
-export interface Firmware {
-  type: string;
-  version: string;
-  config: string;
-  file: string;
-  next?: string;
-}
-
 export interface Certificate {
   extensions?: "server_cert" | "user_cert";
   days?: number;
@@ -14,11 +6,6 @@ export interface Certificate {
 export interface Device {
   secretKey: string;
   certificate?: Certificate;
-  firmware: Array<Firmware>;
 }
 
 export type Devices = Record<string, Device>;
-
-export interface FirmwareParams {
-  version?: string;
-}
